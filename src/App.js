cf3f6b3219587d77841b5d13b5b1ccdf4f3ddb86
Felipe Martins <felipewmartins@gmail.com>
2019-08-25 14:35:10 -0300

    Inicio do service de consumo de dados da api da rocketseat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-
+import './styles.css';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header/>
+    <Main/>
   </div>
 );
 
